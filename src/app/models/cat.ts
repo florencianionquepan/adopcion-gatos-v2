d93046263c6766b9972f966083500ba33d7b0732
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-10 19:41:02 -0300

    Create model for cat

---
+++ src/app/models/cat.ts
@@ -0,0 +1,15 @@
+export interface Cat{
+    "id":number;
+    "nombre":String,
+    "srcFoto":[],
+    "edad":String,
+    "sexo":String,
+    "descripcion":String,
+    "raza":String,
+    "color":String,
+    "tipoPelo":String,
+    "esterilizacion":boolean,
+    "desparasitacion":boolean,
+    "solicitantes":[]
+}
+
