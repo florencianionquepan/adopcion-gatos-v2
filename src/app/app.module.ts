b6fad572514770c8e822e734bf58fe3e73c7a3ef
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-10 17:29:43 -0300

    Crea componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { OldestComponent } from './components/oldest/oldest.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    OldestComponent,
+    CardsComponent,
+    FooterComponent,
+    MainComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/oldest/oldest.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oldest',
+  templateUrl: './oldest.component.html',
+  styleUrls: ['./oldest.component.css']
+})
+export class OldestComponent {
+
+}
