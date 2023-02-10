6cdaeecee660f282f4c530716e8e4562ba55a079
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-10 19:40:34 -0300

    Create cards template and logic

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Cat } from 'src/app/models/cat';
 
 @Component({
   selector: 'app-cards',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
+  @Input() cat:Cat
 
+  constructor(){
+    this.cat={id:1,nombre:"","srcFoto":[],"edad":"","sexo":"","descripcion":"","raza":"",
+              "color":"","tipoPelo":"","esterilizacion":false,"desparasitacion":false,"solicitantes":[]}
+  }
 }
