6ec1ba6418c93b523a94d46f2d69513b16eabb07
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-10 20:21:46 -0300

    Create cat service

---
+++ src/app/services/cat-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cat } from '../models/cat';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatServiceService {
+  headers = new Headers({ 'Accept': 'application/json', 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+  public cats:Cat[];
+  public apiGatos="http://localhost:3000/gato";
+  constructor(private http:HttpClient) {
+    this.cats=[];
+   }
+
+  public verGatos():Observable<Cat[]>{
+    return this.http.get<any>(this.apiGatos);
+  }
+
+
+}
