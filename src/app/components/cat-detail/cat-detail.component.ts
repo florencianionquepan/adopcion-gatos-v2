89c5919fdccae4a4fa50eccfd70e3afb95ef6e03
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-20 23:18:49 -0300

    Create CatDetail Component

---
+++ src/app/components/cat-detail/cat-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-detail',
+  templateUrl: './cat-detail.component.html',
+  styleUrls: ['./cat-detail.component.css']
+})
+export class CatDetailComponent {
+
+}
