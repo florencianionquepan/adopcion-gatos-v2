c07dfd2cd029940f176ac6fed0aea6da1a2716e0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-20 23:25:16 -0300

    Add path detail/id to CatDetail Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatDetailComponent } from './components/cat-detail/cat-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'detail/:id',component:CatDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
