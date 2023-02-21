b35ae7613d33fcd43f849d672dbadd9f8278bf3c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-21 00:29:18 -0300

    Add CatDetail Component logic

--- src/app/components/cat-detail/cat-detail.component.ts
+++ src/app/components/cat-detail/cat-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Cat } from 'src/app/models/cat';
+import { CatServiceService } from 'src/app/services/cat-service.service';
 
 @Component({
   selector: 'app-cat-detail',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./cat-detail.component.css']
 })
 export class CatDetailComponent {
+  public cat:Cat;
 
+  constructor(private ruta: ActivatedRoute, private catSvc: CatServiceService){
+    this.cat={id:0,nombre:"","srcFoto":[],"edad":"","sexo":"","descripcion":"","raza":"",
+    "color":"","tipoPelo":"","esterilizacion":false,"desparasitacion":false,"solicitantes":[]}
+    this.getCat();
+  }
+
+  getCat():void{
+    const id= this.ruta.snapshot.params['id'];
+    this.catSvc.getCat(id).subscribe(kitty=>{
+      this.cat=kitty;
+    })
+  }
+  
 }
