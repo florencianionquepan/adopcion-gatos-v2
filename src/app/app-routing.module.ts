dc401faab963af2601c1c6a29b1955f33206c85b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-21 00:28:24 -0300

    Arrange routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatDetailComponent } from './components/cat-detail/cat-detail.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  {path:'', component:HomeComponent},
   {path:'detail/:id',component:CatDetailComponent}
 ];
 
