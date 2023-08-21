3bd6863fce54799b92aafadb75cf7fa1e7823e0e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-20 21:55:47 -0300

    agrupo gatos en componente cards

--- src/app/public/components/cards/cards.component.ts
+++ src/app/public/components/cards/cards.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Gato } from 'src/app/models/gato';
+import { CatServiceService } from 'src/app/services/cat-service.service';
 
 @Component({
   selector: 'app-cards',
@@ -8,24 +8,29 @@ import { Gato } from 'src/app/models/gato';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
-  @Input() cat:Gato
+  public cats:Gato[]=[];
 
-  constructor(private ruta: Router){
-    this.cat={id:0,nombre:"","fotos":[],"edad":"","sexo":"","descripcion":"",
-              "color":"","tipoPelo":"","adoptado":false,"solicitudes":[]}
+  constructor(private gatoSvc:CatServiceService){
+/*     this.cat={id:0,nombre:"","fotos":[],"edad":"","sexo":"","descripcion":"",
+              "color":"","tipoPelo":"","adoptado":false,"solicitudes":[]} */
   }
 
-  ngOnInit(){
-
+  ngOnInit(): void {
+    // Hacer la solicitud y suscribirse al observable
+    this.gatoSvc.verGatos().subscribe(response => {
+      if (response.Success) {
+        this.cats = response.data.reverse(); // Asignamos los datos al arreglo de gatos
+      }
+    });
   }
-
+/* 
   public detail(cat: Gato){
     this.ruta.navigate([`/detail/${cat.id}`]);
   }
 
   public edit(cat: Gato){
     this.ruta.navigate([`/edit/${cat.id}`]);
-  }
+  } */
 
 
 }
