f191eba879d6f02bb6fedf8bcfe9a1b165b76168
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-25 17:57:50 -0300

    Create backoffice.module.ts

---
+++ src/app/backoffice/backoffice.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BackofficeModule { }
