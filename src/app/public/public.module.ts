4f2cdd68ab6c5359447b0383995a514e6c9541f2
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-25 17:53:58 -0300

    agrega about page al modulo public

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -6,8 +6,7 @@ import { FooterComponent } from '../shared/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CatDetailComponent } from './components/cat-detail/cat-detail.component';
-
-
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +15,8 @@ import { CatDetailComponent } from './components/cat-detail/cat-detail.component
     FooterComponent,
     MainComponent,
     HomeComponent,
-    CatDetailComponent
+    CatDetailComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule
