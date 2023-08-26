8c8587346cb860b4edc0de9d3e816c13b52e45e1
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-26 12:39:17 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { CatDetailComponent } from './public/components/cat-detail/cat-detail.component';
-import { HomeComponent } from './public/pages/home/home.component';
-import { AboutComponent } from './public/pages/about/about.component';
-import { ProfileComponent } from './backoffice/pages/profile/profile.component';
-import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {path:'', 
-    component:HomeComponent},
-  {path:'about',
-    component:AboutComponent},
-  {path:'login',
-  component:LoginComponent
+    loadChildren: ()=>import('./public/public.module').then(m=>m.PublicModule)
   },
-  {path:'cats/:id',
-    component:CatDetailComponent},
   {path:'backoffice',
     loadChildren: ()=>import('./backoffice/backoffice.module').then(m=>m.BackofficeModule)
   },
   {
-    path:'**',
-    redirectTo:"/"
+    path: '**',
+    redirectTo: '',
   }
 ];
 
