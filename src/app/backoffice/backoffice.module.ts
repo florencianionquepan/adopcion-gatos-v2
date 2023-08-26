b71a2ac1db90064ef49dfeec31b37b54b4704cc3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-25 21:34:47 -0300

    Update backoffice.module.ts

--- src/app/backoffice/backoffice.module.ts
+++ src/app/backoffice/backoffice.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RouterModule } from '@angular/router';
+import { BackofficeRoutingModule } from './backoffice-routing.module';
 
 
 
 @NgModule({
   declarations: [
+    ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BackofficeRoutingModule
   ]
 })
 export class BackofficeModule { }
