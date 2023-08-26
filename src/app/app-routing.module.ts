6e8b3f601da7069057ba0767e98f8b53b9879299
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-25 21:35:34 -0300

    agrega otras rutas y la de backoffice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { CatDetailComponent } from './public/components/cat-detail/cat-detail.component';
 import { HomeComponent } from './public/pages/home/home.component';
+import { AboutComponent } from './public/pages/about/about.component';
+import { ProfileComponent } from './backoffice/pages/profile/profile.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent},
-  {path:'detail/:id',component:CatDetailComponent}
+  {path:'', 
+    component:HomeComponent},
+  {path:'about',
+    component:AboutComponent},
+  {path:'cats/:id',
+    component:CatDetailComponent},
+  {path:'backoffice',
+    loadChildren: ()=>import('./backoffice/backoffice.module').then(m=>m.BackofficeModule)
+  }
 ];
 
 @NgModule({
