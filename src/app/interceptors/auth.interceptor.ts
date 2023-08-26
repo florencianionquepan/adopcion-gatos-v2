fc7016ad9173d443ace12a5ad9fd116a939bb9fe
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-26 10:37:45 -0300

    Create auth.interceptor.ts

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpHeaders,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { User } from '../models/user';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  user=new User();
+
+  constructor(private router:Router) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler){
+    let httpHeaders=new HttpHeaders();
+    if(sessionStorage.getItem('userDetails')){
+      this.user=JSON.parse(sessionStorage.getItem('userdetails')!);
+    }
+    if(this.user && this.user.password && this.user.passwordC && this.user.email){
+      httpHeaders = httpHeaders.append('Authorization','Basic '+window.btoa(this.user.email + ':' + this.user.password));
+    }else{
+      let authorization=sessionStorage.getItem('Authorization');
+      if(authorization){
+        httpHeaders = httpHeaders.append('Authorization', authorization); 
+      }
+    }
+    let xsrf=sessionStorage.getItem('XSRF-TOKEN');
+    if(xsrf){
+      httpHeaders = httpHeaders.append('X-XSRF-TOKEN', xsrf);  
+    }
+    httpHeaders = httpHeaders.append('X-Requested-With', 'XMLHttpRequest');
+    const xhr = req.clone({
+      headers: httpHeaders
+    });
+    return next.handle(xhr).pipe(tap(
+      (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status !== 401) {
+            return;
+          }
+          this.router.navigate(['/']);
+        }
+      }));
+  }
+}
