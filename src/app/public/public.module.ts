ead5008ce2e67cd7df58ab86635f5ce0902917fd
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-26 12:39:37 -0300

    Update public.module.ts

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,29 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../shared/header/header.component';
 import { CardsComponent } from './components/cards/cards.component';
-import { FooterComponent } from '../shared/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CatDetailComponent } from './components/cat-detail/cat-detail.component';
 import { AboutComponent } from './pages/about/about.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { PublicRoutingModule } from './public-routing.module';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
     CardsComponent,
-    FooterComponent,
     MainComponent,
     HomeComponent,
     CatDetailComponent,
     AboutComponent
   ],
   imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent
+    CommonModule,
+    RouterModule,
+    PublicRoutingModule,
+    SharedModule
   ]
 })
 export class PublicModule { }
