29158910a3d75d8600c3e3e4c42c20293e5fefaf
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-26 09:19:53 -0300

    agrega rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,24 @@ import { CatDetailComponent } from './public/components/cat-detail/cat-detail.co
 import { HomeComponent } from './public/pages/home/home.component';
 import { AboutComponent } from './public/pages/about/about.component';
 import { ProfileComponent } from './backoffice/pages/profile/profile.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {path:'', 
     component:HomeComponent},
   {path:'about',
     component:AboutComponent},
+  {path:'login',
+  component:LoginComponent
+  },
   {path:'cats/:id',
     component:CatDetailComponent},
   {path:'backoffice',
     loadChildren: ()=>import('./backoffice/backoffice.module').then(m=>m.BackofficeModule)
+  },
+  {
+    path:'**',
+    redirectTo:"/"
   }
 ];
 
