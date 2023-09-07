f099ea8cfcbef8315bda34b43db44383c708d07a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-06 22:43:03 -0300

    Create GatoDetalle.ts

---
+++ src/app/models/GatoDetalle.ts
@@ -0,0 +1,48 @@
+import { FichaVeterinaria } from "./FichaVeterinaria";
+import { Padrino } from "./Padrino";
+
+export class GatoDetalle{
+    "id":number;
+    "nombre":string;
+    "fotos":string[];
+    "edad":string;
+    "sexo":string;
+    "descripcion":string;
+    "color":string;
+    "tipoPelo":string;
+    "padrino":Padrino;
+    "adoptado":Date | null;
+    "fichaVeterinaria":FichaVeterinaria;
+    "montoMensual":number;
+    "solicitudes":string[];
+
+    constructor(
+        id?: number,
+        nombre?: string,
+        fotos?: string[],
+        edad?: string,
+        sexo?: string,
+        descripcion?: string,
+        color?: string,
+        tipoPelo?: string,
+        padrino: Padrino =new Padrino(),
+        adoptado?: Date | null,
+        fichaVeterinaria: FichaVeterinaria =new FichaVeterinaria (),
+        montoMensual?: number,
+        solicitudes?: string[]
+    ) {
+        this.id = id || 0;
+        this.nombre = nombre || '';
+        this.fotos = fotos || [];
+        this.edad = edad || '';
+        this.sexo = sexo || '';
+        this.descripcion = descripcion || '';
+        this.color = color || '';
+        this.tipoPelo = tipoPelo || '';
+        this.padrino = padrino;
+        this.adoptado = adoptado || null;
+        this.fichaVeterinaria = fichaVeterinaria;
+        this.montoMensual = montoMensual || 0;
+        this.solicitudes = solicitudes || [];
+    }
+}
