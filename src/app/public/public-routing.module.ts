5d63879228c1168e8945fa39650d56b98f160f38
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-06 22:45:25 -0300

    Update public-routing.module.ts

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
-import { LoginComponent } from '../auth/login/login.component';
-import { CatDetailComponent } from '../shared/components/cat-detail/cat-detail.component';
-import { HomeComponent } from '../shared/pages/home/home.component';
 
 const routes: Routes = [
   {
