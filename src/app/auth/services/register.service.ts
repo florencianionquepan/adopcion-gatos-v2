d51c964000613c8d7dc1447b6ff19a86db83306c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-08 18:56:15 -0300

    Update register.service.ts

--- src/app/auth/services/register.service.ts
+++ src/app/auth/services/register.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Registro } from 'src/app/models/Registro';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  public id:number;
+  public token:string;
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.id=0;
+    this.token='';
+   }
+
+  register(registro:Registro):Observable<any>{
+    return this.http.post(`${environment.url}/personas`,registro)
+    .pipe(
+      catchError(err=>{
+        //console.log(err);
+        return throwError(()=>err.error)
+      })
+    )
+  }
+
+  validate(id:number, token:string):Observable<any>{
+    return this.http.get(`${environment.url}/usuarios/${id}/validacion/${token}`)
+    .pipe(
+      catchError(err=>{
+        console.log(err);
+        return throwError(()=>err.error)
+      })
+    )
+  }
 }
