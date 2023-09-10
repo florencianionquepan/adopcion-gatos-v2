86072b586c6c0db0d555cc9bc6d08592e682ce3d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-10 14:31:16 -0300

    Update validation.component.ts

--- src/app/auth/validation/validation.component.ts
+++ src/app/auth/validation/validation.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { RegisterService } from '../services/register.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-validation',
@@ -10,8 +12,31 @@ export class ValidationComponent {
   public id:number;
   public token:string;
 
-  constructor(private router:ActivatedRoute){
+  constructor(private router:ActivatedRoute,private service: RegisterService){
     this.id=this.router.snapshot.params['id'];
     this.token=this.router.snapshot.params['token'];
   }
+
+  ngOnInit():void{
+    this.service.validate(this.id,this.token)
+    .subscribe({
+      next:(response)=>{
+        console.log(response);
+        if(response.success){
+          Swal.fire({
+            title:"OK",
+            text:response.data,
+            icon:'success'
+          });
+          }
+        }
+        ,error:(e)=>{
+          Swal.fire({'title':e.estado,
+          'text':e.mensaje,
+          icon:'error'});
+        }
+      })
+      
+    }
+
 }
