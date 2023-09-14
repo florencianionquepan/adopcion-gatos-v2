f1d383f5b6893a89c0059b1f220b9b00d2c4efb7
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-14 19:23:51 -0300

    Create Transito.ts

---
+++ src/app/models/Transito.ts
@@ -0,0 +1,22 @@
+export class Transito{
+    public id:number;
+    public dni:string;
+    public nombre:string;
+    public email:string;
+    public fechaNacimiento:Date;
+    public telefono:string;
+    public direccion:string;
+    public localidad:string;
+
+    constructor(id?:number,dni?:string,nombre?:string,email?:string,fechaNacimiento?:Date,
+        telefono?:string,direccion?:string,localidad?:string){
+            this.id = id || 0;
+            this.dni = dni || '';
+            this.nombre = nombre || '';
+            this.email = email || '';
+            this.fechaNacimiento = fechaNacimiento || new Date() ;
+            this.telefono = telefono || '';
+            this.direccion = direccion || '';
+            this.localidad = localidad || '';
+    }
+}
