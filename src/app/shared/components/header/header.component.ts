0783191516adaa1d1044272735fdfc7108a0a1f0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-14 19:21:09 -0300

    Creo todos los links para header segun rol

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent {
+  user=new User();
+  public list: any[]=[]
+
+  constructor(private router : Router) { 
+
+  }
+
+  ngOnInit() {
+    if(sessionStorage.getItem('userdetails')){
+      this.user = JSON.parse(sessionStorage.getItem('userdetails')!);
+      this.crearLinks();
+    }
+  }
+
+  logout(){
+    sessionStorage.clear();
+    this.router.navigate(['/']);
+  }
+
+  crearLinks():void{
+    const roleToLinks:any = {
+      'ROLE_USER': [
+      {
+        text:'Mi perfil',
+        link:'backoffice/perfil',
+        render:true
+      },
+      {
+        text: 'Mis solicitudes',
+        link: 'backoffice/missolicitudes',
+        render: true
+      }],
+      'ROLE_VOLUNTARIO': [{
+        text: 'Mis gatos',
+        link: 'backoffice/misgatos',
+        render: true
+      }],
+      'ROLE_SOCIO': [
+        {
+          text: 'Solicitudes',
+          link: 'backoffice/solicitudes',
+          render: true
+        },
+        {
+          text: 'Usuarios',
+          link: 'backoffice/usuarios',
+          render: true
+        }
+      ]
+    };
+  
+    const userRoles = this.user.roles.map(rol => rol.nombre);
+  
+    userRoles.forEach(role => {
+      if (roleToLinks[role]) {
+        this.list.push(...roleToLinks[role]);
+      }
+    });
+    console.log(this.list);
+  }
 
 }
