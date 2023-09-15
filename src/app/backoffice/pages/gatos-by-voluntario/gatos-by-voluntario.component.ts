547ebc0db77e92b09622b165adb80f154f42293b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-14 21:10:20 -0300

    Update gatos-by-voluntario.component.ts

--- src/app/backoffice/pages/gatos-by-voluntario/gatos-by-voluntario.component.ts
+++ src/app/backoffice/pages/gatos-by-voluntario/gatos-by-voluntario.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { GatoDetalle } from 'src/app/models/GatoDetalle';
+import { User } from 'src/app/models/user';
+import { GatosService } from 'src/app/services/gatos.service';
 
 @Component({
   selector: 'app-gatos-by-voluntario',
@@ -7,5 +9,32 @@ import { GatoDetalle } from 'src/app/models/GatoDetalle';
   styleUrls: ['./gatos-by-voluntario.component.css']
 })
 export class GatosByVoluntarioComponent {
+  user=new User();
   public gatos:GatoDetalle[]=[];
+
+  constructor(private gatoSer:GatosService){
+
+  }
+
+  ngOnInit(){
+    if(sessionStorage.getItem('userdetails')){
+      this.user = JSON.parse(sessionStorage.getItem('userdetails')!);
+    }
+    this.getGatos();
+  }
+
+  public getGatos():void{
+    this.gatoSer.gatosByVoluntario(this.user.email)
+    .subscribe({
+      next:(response)=>{
+        console.log(response.data);
+        //this.gatos=response.data;
+      },
+      error:(e)=>{
+        console.error("Error al obtener los datos", e);
+      }
+    })
+  }
+
+
 }
