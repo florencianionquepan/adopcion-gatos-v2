a8d602ceffcec55303f1a4739303990b9f875fa5
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-19 18:20:34 -0300

    crea componente ficha gato

---
+++ src/app/backoffice/components/ficha-gato/ficha-gato.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ficha-gato',
+  templateUrl: './ficha-gato.component.html',
+  styleUrls: ['./ficha-gato.component.css']
+})
+export class FichaGatoComponent {
+
+}
