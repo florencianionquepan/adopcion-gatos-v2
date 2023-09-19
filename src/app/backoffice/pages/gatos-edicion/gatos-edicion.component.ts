a44b0d89533b53f210c4814818f94dae5459d346
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-19 18:20:50 -0300

    crea pagina edición de gato

---
+++ src/app/backoffice/pages/gatos-edicion/gatos-edicion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gatos-edicion',
+  templateUrl: './gatos-edicion.component.html',
+  styleUrls: ['./gatos-edicion.component.css']
+})
+export class GatosEdicionComponent {
+
+}
