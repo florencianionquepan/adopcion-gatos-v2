c7480b4ca53e3461b753dde2e74d8d017adb874f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-19 23:28:28 -0300

    crea page de ficha

---
+++ src/app/backoffice/pages/ficha-page/ficha-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ficha-page',
+  templateUrl: './ficha-page.component.html',
+  styleUrls: ['./ficha-page.component.css']
+})
+export class FichaPageComponent {
+
+}
