5e384e8d9a4928ee714631d5bc2bdff8e2292503
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-23 20:03:26 -0300

    obtiene ficha si existe al ingresar al form de ficha

--- src/app/backoffice/pages/ficha-page/ficha-page.component.ts
+++ src/app/backoffice/pages/ficha-page/ficha-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FichaVeterinaria } from 'src/app/models/FichaVeterinaria';
+import { GatosService } from 'src/app/services/gatos.service';
 
 @Component({
   selector: 'app-ficha-page',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./ficha-page.component.css']
 })
 export class FichaPageComponent {
+  ficha:FichaVeterinaria=new FichaVeterinaria();
+
+  constructor(private service:GatosService, 
+    private ruta: ActivatedRoute){
+  }
+
+  ngOnInit():void{
+    this.getFicha();
+  }
+
+  getFicha():void{
+    const id= this.ruta.snapshot.params['id'];
+    this.service.getFicha(id).subscribe(resp=>{
+      //console.log(resp);
+      //si no existe la ficha data=null
+      this.ficha=resp.data;
+    })
+  }
 
 }
