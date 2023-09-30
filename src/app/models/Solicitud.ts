c7d47b5c8506d8b45c523c9308cb47116dd33a63
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-30 20:03:26 -0300

    Update Solicitud.ts

--- src/app/models/Solicitud.ts
+++ src/app/models/Solicitud.ts
@@ -1,15 +1,20 @@
 import { Estado } from "./Estado";
+import { GatoDetalle } from "./GatoDetalle";
+import { Persona } from "./Persona";
+import { Gato } from "./gato";
 
 export class Solicitud{
     public id:number;
     public estados:Estado[];
-    public idGato:number;
+    public gato:GatoDetalle;
+    public solicitante:Persona;
     public motivo:string;
 
-    constructor(id?: number,estados?: Estado[],idGato?: number,motivo?: string) {
+    constructor(id?: number,estados?: Estado[],gato?: GatoDetalle, solicitante?:Persona,motivo?: string) {
         this.id = id || 0;
         this.estados = estados || [];
-        this.idGato = idGato || 0;
+        this.gato = gato || new GatoDetalle();
+        this.solicitante = solicitante || new Persona();
         this.motivo = motivo || '';
     }
 }
