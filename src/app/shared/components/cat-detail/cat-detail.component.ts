c739345d6b9e407381e0e6a0bff89d6f4c30d35e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-30 19:13:31 -0300

    Update cat-detail.component.ts

--- src/app/shared/components/cat-detail/cat-detail.component.ts
+++ src/app/shared/components/cat-detail/cat-detail.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GatoDetalle } from 'src/app/models/GatoDetalle';
 import { GatosService } from 'src/app/services/gatos.service';
+import { AuthService } from '../../services/auth.service';
+import { User } from 'src/app/models/user';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cat-detail',
@@ -10,9 +13,16 @@ import { GatosService } from 'src/app/services/gatos.service';
 })
 export class CatDetailComponent {
   gato=new GatoDetalle();
+  user=new User();
 
-  constructor(private ruta: ActivatedRoute, private catSvc: GatosService){
+  constructor(private ruta: ActivatedRoute,
+            private catSvc: GatosService,
+            private authSvc:AuthService){
     this.getCat();
+  }
+
+  ngOnInit(){
+    this.user=this.authSvc.user;
   }
 
   //agregar manejo errores
@@ -22,6 +32,39 @@ export class CatDetailComponent {
       //console.log(resp);
       this.gato=resp.data;
     })
+  }
+
+  adoptar(gato:GatoDetalle):void{
+    if(this.user.authStatus){
+      this.alertAdopcion();
+    }else{
+      this.alertLogin();
+    }
+  }
+
+  alertAdopcion():void{
+    Swal.fire({
+      title: 'Estas seguro de enviar tu solicitud de adopcion?',
+      icon: 'info',
+      cancelButtonText:'Cancelar',
+      confirmButtonText: 'Enviar Solicitud'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.enviarSolicitud(this.gato,this.user);
+      }
+    })
+  }
+
+  enviarSolicitud(gato:GatoDetalle, user:User):void{
+
+  }
+
+  apadrinar(gato:GatoDetalle):void{
+
+  }
+
+  alertLogin():void{
+    this.authSvc.alertLogin();
   }
   
 }
