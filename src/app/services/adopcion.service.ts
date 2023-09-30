26cf8557ad234f69f7bb22b5622cc2b7ecbd238f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-30 20:05:03 -0300

    Update adopcion.service.ts

--- src/app/services/adopcion.service.ts
+++ src/app/services/adopcion.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Solicitud } from '../models/Solicitud';
+import { Persona } from '../models/Persona';
+import { GatoDetalle } from '../models/GatoDetalle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdopcionService {
+  public apiSolicitud=`${environment.url}/solicitudes`;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public enviarSolicitud(idGato:number, email:string):Observable<any>{
+    const gato=new GatoDetalle();
+    gato.id=idGato;
+    const solicitante=new Persona();
+    solicitante.email=email;
+    const nueva=new Solicitud(0,[],gato,solicitante);
+    return this.http.post(this.apiSolicitud,nueva,{withCredentials:true})
+    .pipe(
+      catchError(err=>{
+        console.log(err);
+        return throwError(()=>err.error)
+      })
+    )
+  }
 }
