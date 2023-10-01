db14b96b1f07764cb1e0201cd82fd9251f78b858
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-01 18:26:52 -0300

    Update notification.component.ts

--- src/app/shared/components/notification/notification.component.ts
+++ src/app/shared/components/notification/notification.component.ts
@@ -1,4 +1,8 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, take } from 'rxjs';
+import { Notificacion } from 'src/app/models/Notificacion';
+import { NotificacionService } from 'src/app/services/notificacion.service';
 declare var bootstrap: any; 
 
 @Component({
@@ -9,10 +13,30 @@ declare var bootstrap: any;
 export class NotificationComponent {
   @ViewChild('toastTrigger') toastTrigger!: ElementRef;
   @ViewChild('toastLiveExample') toastLiveExample!: ElementRef;
+  notificaciones:Notificacion[]=[];
 
   showToast() {
     const toast = new bootstrap.Toast(this.toastLiveExample.nativeElement);
     toast.show();
+  }
+
+  constructor(private router:Router, private service:NotificacionService){
+    this.router.events.pipe(
+      filter((e) => e instanceof NavigationEnd),
+      take(1) // Toma solo el primer evento NavigationEnd
+    ).subscribe(e=>{
+    if(e instanceof NavigationEnd){
+      this.service.listarTodas().subscribe({
+        next:(response)=>{
+          this.notificaciones=response.data;
+          //console.log(this.notificaciones);
+        }
+        ,error:(e)=>{
+          console.log(e);
+        }
+      })
+    }
+   }) 
   }
 
 }
