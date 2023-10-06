ad086d51eefe19105af1780d6b617d3e8d10f13b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-06 17:24:24 -0300

    Crea page asignacion-transito

---
+++ src/app/backoffice/pages/asignacion-transito-page/asignacion-transito-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asignacion-transito-page',
+  templateUrl: './asignacion-transito-page.component.html',
+  styleUrls: ['./asignacion-transito-page.component.css']
+})
+export class AsignacionTransitoPageComponent {
+
+}
