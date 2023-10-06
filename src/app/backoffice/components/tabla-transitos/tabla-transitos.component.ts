6af50779c3e2c9b067556436c7e0596ec1b58df3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-06 18:22:11 -0300

    Creo componente tabla transitos

---
+++ src/app/backoffice/components/tabla-transitos/tabla-transitos.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Transito } from 'src/app/models/Transito';
+import { TransitoService } from 'src/app/services/transito.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-tabla-transitos',
+  templateUrl: './tabla-transitos.component.html',
+  styleUrls: ['./tabla-transitos.component.css']
+})
+export class TablaTransitosComponent {
+transitos: Transito[]=[];
+
+  constructor(private service:TransitoService){
+    this.service.verTransitos()
+    .subscribe({
+      next:(response)=>{
+        this.transitos=response.data;
+      },
+      error:(e)=>{
+        //console.error("Error al enviar la ficha", e);
+        Swal.fire({title:'Error',icon:'error',text:e.mensaje})
+      }
+    }
+    )
+  }
+}
