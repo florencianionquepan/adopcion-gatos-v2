33e6773b00bb9aab750cd886505c498715950f58
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-06 18:35:14 -0300

    ajuste nombre de atributo

--- src/app/models/Transito.ts
+++ src/app/models/Transito.ts
@@ -1,22 +1,29 @@
+import { Gato } from "./gato";
+
 export class Transito{
     public id:number;
     public dni:string;
     public nombre:string;
+    public apellido:string;
     public email:string;
     public fechaNacimiento:Date;
     public telefono:string;
     public direccion:string;
     public localidad:string;
+    public gatos:Gato[];
 
-    constructor(id?:number,dni?:string,nombre?:string,email?:string,fechaNacimiento?:Date,
-        telefono?:string,direccion?:string,localidad?:string){
+    constructor(id?:number,dni?:string,nombre?:string,apellido?:string,
+        email?:string,fechaNacimiento?:Date,
+        telefono?:string,direccion?:string,localidad?:string, gatos?:Gato[]){
             this.id = id || 0;
             this.dni = dni || '';
             this.nombre = nombre || '';
+            this.apellido = apellido || '';
             this.email = email || '';
             this.fechaNacimiento = fechaNacimiento || new Date() ;
             this.telefono = telefono || '';
             this.direccion = direccion || '';
             this.localidad = localidad || '';
+            this.gatos = gatos || [];
     }
 }
