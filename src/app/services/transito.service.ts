62719c76678e12b816d46114434d2f04e35847d9
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-06 18:21:18 -0300

    Create transito.service.ts

---
+++ src/app/services/transito.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransitoService {
+  public apiTransitos=`${environment.url}/transitos`;
+
+  constructor(private http:HttpClient) { }
+
+  public verTransitos():Observable<any>{
+    return this.http.get<any>(this.apiTransitos)
+    .pipe(
+      catchError(err=>{
+        console.log(err);
+        return throwError(()=>err.error)
+      })
+    )
+  }
+}
