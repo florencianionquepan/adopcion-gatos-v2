6e92bff7445784b70379a54b60a4c42a9b9d0677
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-07 17:07:38 -0300

    Update auth.interceptor.ts

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -10,6 +10,7 @@ import {
 import { Observable, tap } from 'rxjs';
 import { User } from '../models/user';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -18,35 +19,40 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private router:Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler){
-    let httpHeaders=new HttpHeaders();
-    if(sessionStorage.getItem('userdetails')){
-      this.user=JSON.parse(sessionStorage.getItem('userdetails')!);
-    }
-    if(this.user && this.user.password && this.user.email){
-      httpHeaders = httpHeaders.append('Authorization', 'Basic ' + window.btoa(this.user.email + ':' + this.user.password));
-      sessionStorage.setItem('userdetails','');
-    }else{
-      let authorization=sessionStorage.getItem('Authorization');
-      if(authorization){
-        httpHeaders = httpHeaders.append('Authorization', authorization); 
+    if(req.url.startsWith(environment.url)){
+      let httpHeaders=new HttpHeaders();
+      if(sessionStorage.getItem('userdetails')){
+        this.user=JSON.parse(sessionStorage.getItem('userdetails')!);
       }
-    }
-    let xsrf = sessionStorage.getItem('XSRF-TOKEN');
-    if(xsrf){
-      httpHeaders = httpHeaders.append('X-XSRF-TOKEN', xsrf);  
-    }
-    httpHeaders = httpHeaders.append('X-Requested-With', 'XMLHttpRequest');
-    const xhr = req.clone({
-      headers: httpHeaders
-    });
-    return next.handle(xhr).pipe(tap(
-      (err: any) => {
-        if (err instanceof HttpErrorResponse) {
-          if (err.status !== 401) {
-            return;
-          }
-          this.router.navigate(['/']);
+      if(this.user && this.user.password && this.user.email){
+        httpHeaders = httpHeaders.append('Authorization', 'Basic ' + window.btoa(this.user.email + ':' + this.user.password));
+        sessionStorage.setItem('userdetails','');
+      }else{
+        let authorization=sessionStorage.getItem('Authorization');
+        if(authorization){
+          httpHeaders = httpHeaders.append('Authorization', authorization); 
         }
-      }));
+      }
+      let xsrf = sessionStorage.getItem('XSRF-TOKEN');
+      if(xsrf){
+        httpHeaders = httpHeaders.append('X-XSRF-TOKEN', xsrf);  
+      }
+      httpHeaders = httpHeaders.append('X-Requested-With', 'XMLHttpRequest');
+      const xhr = req.clone({
+        headers: httpHeaders
+      });
+      return next.handle(xhr).pipe(tap(
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status !== 401) {
+              return;
+            }
+            this.router.navigate(['/']);
+          }
+        }));
+    }else{
+      return next.handle(req);
+    }
   }
+    
 }
