a3a8ab1a0eb9c7c62f2fa9d3cf7329ef81caab98
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-07 19:46:31 -0300

    vista y logica de tabla de solicitudes

--- src/app/backoffice/components/tabla-solicitudes-adopcion/tabla-solicitudes-adopcion.component.ts
+++ src/app/backoffice/components/tabla-solicitudes-adopcion/tabla-solicitudes-adopcion.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Solicitud } from 'src/app/models/Solicitud';
+import { AdopcionService } from 'src/app/services/adopcion.service';
 
 @Component({
   selector: 'app-tabla-solicitudes-adopcion',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabla-solicitudes-adopcion.component.css']
 })
 export class TablaSolicitudesAdopcionComponent {
+  idGato:number;
+  solicitudes:Solicitud[]=[];
+  
 
+  constructor(private service:AdopcionService,
+            private actiRoute:ActivatedRoute){
+    this.idGato=this.actiRoute.snapshot.params['id'];
+    this.service.listarByGato(this.idGato).subscribe(
+      (response)=>{
+        this.solicitudes=response;
+      }
+    )
+
+  }
+
+  aceptar(){
+
+  }
+
+  rechazar(){
+
+  }
 }
