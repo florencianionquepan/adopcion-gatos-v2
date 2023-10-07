1dbcf1c34ad0c1e8ebcd115c9602a149c5f60062
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-07 17:42:11 -0300

    Creación componente tabla solicitudes adopción

---
+++ src/app/backoffice/components/tabla-solicitudes-adopcion/tabla-solicitudes-adopcion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-solicitudes-adopcion',
+  templateUrl: './tabla-solicitudes-adopcion.component.html',
+  styleUrls: ['./tabla-solicitudes-adopcion.component.css']
+})
+export class TablaSolicitudesAdopcionComponent {
+
+}
