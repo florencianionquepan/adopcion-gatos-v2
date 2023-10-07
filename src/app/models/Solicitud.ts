2a0eda4c29301f69b2d2a9b7f512e2f1de91b09f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-07 19:44:55 -0300

    ajuste de modelos

--- src/app/models/Solicitud.ts
+++ src/app/models/Solicitud.ts
@@ -1,20 +1,18 @@
 import { Estado } from "./Estado";
 import { GatoDetalle } from "./GatoDetalle";
 import { Persona } from "./Persona";
-import { Gato } from "./gato";
 
+//su usa cuando se envia una solicitud de adopcion!
 export class Solicitud{
     public id:number;
     public estados:Estado[];
     public gato:GatoDetalle;
     public solicitante:Persona;
-    public motivo:string;
 
-    constructor(id?: number,estados?: Estado[],gato?: GatoDetalle, solicitante?:Persona,motivo?: string) {
+    constructor(id?: number,estados?: Estado[],gato?: GatoDetalle, solicitante?:Persona) {
         this.id = id || 0;
         this.estados = estados || [];
         this.gato = gato || new GatoDetalle();
         this.solicitante = solicitante || new Persona();
-        this.motivo = motivo || '';
     }
 }
