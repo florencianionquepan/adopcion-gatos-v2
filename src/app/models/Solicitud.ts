63f727c900ce030578a46583a8ba46680153bf7e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-13 00:04:56 -0300

    Update Solicitud.ts

--- src/app/models/Solicitud.ts
+++ src/app/models/Solicitud.ts
@@ -3,16 +3,19 @@ import { GatoDetalle } from "./GatoDetalle";
 import { Persona } from "./Persona";
 
 //su usa cuando se envia una solicitud de adopcion!
+//para actualizar solicitud el dto usa el motivo aca!!
 export class Solicitud{
     public id:number;
     public estados:Estado[];
     public gato:GatoDetalle;
     public solicitante:Persona;
+    public motivo:string;
 
-    constructor(id?: number,estados?: Estado[],gato?: GatoDetalle, solicitante?:Persona) {
+    constructor(id?: number,estados?: Estado[],gato?: GatoDetalle, solicitante?:Persona, motivo?:string) {
         this.id = id || 0;
         this.estados = estados || [];
         this.gato = gato || new GatoDetalle();
         this.solicitante = solicitante || new Persona();
+        this.motivo= motivo || '';
     }
 }
