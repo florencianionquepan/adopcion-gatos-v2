fb856b7b72c6de5f50eb50d17f9c8bb8d1bc9c7f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-13 22:15:00 -0300

    Create persona.service.ts

---
+++ src/app/services/persona.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaService {
+  public apiNotificaciones=`${environment.url}/personas`;
+  constructor(private http:HttpClient) { }
+
+  public listaVoluntariados(dni:string):Observable<any>{
+    return this.http.get(`${this.apiNotificaciones}/${dni}/voluntariado`).pipe(
+      map((response:any) => {
+        if (response.success) {
+          return response.data; 
+        } else {
+          throw new Error(response);
+        }
+      }),
+      catchError((error: any) => {
+        throw error;
+      })
+    )
+  }
+}
