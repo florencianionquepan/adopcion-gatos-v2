62ed5c7b1997f63ce6e1672ede12165e49b41185
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-14 19:52:39 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,8 @@ export const environment={
     production:false,
     url:"http://localhost:9090"
 } 
+
+export const apiGeoRef={
+    production:false,
+    url:"https://apis.datos.gob.ar/georef/api"
+}
