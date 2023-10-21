1f4ede30a3fc1c9cb1e6e695aa7cebab78b69584
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-21 18:04:33 -0300

    agrego tabla y listo datos de mis solicitudes

--- src/app/backoffice/components/missolicitudes/missolicitudes.component.ts
+++ src/app/backoffice/components/missolicitudes/missolicitudes.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Solicitud } from 'src/app/models/Solicitud';
+import { User } from 'src/app/models/user';
+import { AdopcionService } from 'src/app/services/adopcion.service';
 
 @Component({
   selector: 'app-missolicitudes',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./missolicitudes.component.css']
 })
 export class MissolicitudesComponent {
+  solicitudes:Solicitud[]=[];
+  user:User=new User();
 
+  constructor(private service:AdopcionService){
+    if(sessionStorage.getItem('userdetails')){
+      this.user = JSON.parse(sessionStorage.getItem('userdetails')!);
+    }
+  }
+
+  ngOnInit(){
+    this.service.listarSoliBySolicitante(this.user.email).subscribe(
+      (data)=>{
+        console.log(data);
+        this.solicitudes=data;
+      }
+    )
+  }
 }
