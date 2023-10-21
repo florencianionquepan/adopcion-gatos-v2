4e71912a569a4564a9c0163d62dc9f58b801a604
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-21 18:04:48 -0300

    creo componente de titulo-page reutilizable

---
+++ src/app/backoffice/components/titulo-page/titulo-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo-page',
+  templateUrl: './titulo-page.component.html',
+  styleUrls: ['./titulo-page.component.css']
+})
+export class TituloPageComponent {
+  @Input() titulo:string='';
+  @Input() link:string='';
+}
