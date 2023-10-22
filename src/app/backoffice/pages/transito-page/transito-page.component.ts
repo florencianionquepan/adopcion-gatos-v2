d155b5d74b618650457b6f206181290b61cf4651
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-22 19:35:13 -0300

    crep page gatosentransito

---
+++ src/app/backoffice/pages/transito-page/transito-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transito-page',
+  templateUrl: './transito-page.component.html',
+  styleUrls: ['./transito-page.component.css']
+})
+export class TransitoPageComponent {
+
+}
