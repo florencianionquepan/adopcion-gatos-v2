c7827ecc5e93cbd1b83cf5ad0a536482db9d4c93
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-22 21:00:36 -0300

    llamada http para obtener gatos de transito

--- src/app/services/transito.service.ts
+++ src/app/services/transito.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, map, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -17,6 +17,21 @@ export class TransitoService {
       catchError(err=>{
         console.log(err);
         return throwError(()=>err.error);
+      })
+    )
+  }
+
+  public gatosByTransito(email:string):Observable<any>{
+    return this.http.get<any>(`${this.apiTransitos}/${email}/gatos`).pipe(
+      map((response:any) => {
+        if (response.success) {
+          return response.data; 
+        } else {
+          throw new Error(response);
+        }
+      }),
+      catchError((error: any) => {
+        throw error;
       })
     )
   }
