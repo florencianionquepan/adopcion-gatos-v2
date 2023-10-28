f1a1781e2e1c93d9e5e512332b3a0c7046336e2f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-28 15:37:53 -0300

    Create padrino.service.ts

---
+++ src/app/services/padrino.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Cuota } from '../models/Cuota';
+import { GatoDetalle } from '../models/GatoDetalle';
+import { Padrino } from '../models/Padrino';
+import { Observable, catchError, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PadrinoService {
+  public apiCuotas=`${environment.url}/cuotas`;
+
+  constructor(private http:HttpClient) { }
+
+  pagarCuota(email:string,gato:GatoDetalle):Observable<any>{
+    let cuota=new Cuota();
+    let padrino=new Padrino();
+    padrino.email=email;
+    cuota.padrino=padrino;
+    cuota.gato=gato;
+    cuota.monto=gato.montoMensual;
+    return this.http.post(`${this.apiCuotas}`,cuota).pipe(
+      map((response:any) => {
+        if (response.success) {
+          return response.data; 
+        } else {
+          throw new Error(response);
+        }
+      }),
+      catchError((error: any) => {
+        throw error;
+      })
+    )
+  }
+
+
+}
