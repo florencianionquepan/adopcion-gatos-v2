b66190b74eb0ac21f7bf8350c3b794274d9c1acb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-31 21:11:56 -0300

    agrego llamada a listarcuotas

--- src/app/services/padrino.service.ts
+++ src/app/services/padrino.service.ts
@@ -14,18 +14,35 @@ export class PadrinoService {
 
   constructor(private http:HttpClient) {}
 
+  //la primera vez que paga una cuota
   pagarCuota(email:string,gato:GatoDetalle):Observable<any>{
     let cuota=new Cuota();
     let padrino=new Padrino();
     padrino.email=email;
     cuota.padrino=padrino;
     cuota.gato=gato;
-    cuota.monto=gato.montoMensual;
+    cuota.montoMensual=gato.montoMensual;
     return this.http.post(`${this.apiCuotas}`,cuota).pipe(
       map((response:any) => {
         if (response.success) {
           //console.log(response.data);
           window.open(response.data,'_blank');
+        } else {
+          throw new Error(response);
+        }
+      }),
+      catchError((error: any) => {
+        throw error;
+      })
+    )
+  }
+
+  getCuotasByEmail(email:string):Observable<any>{
+    return this.http.get(`${this.apiCuotas}/padrino/${email}`).pipe(
+      map((response:any) => {
+        if (response.success) {
+          //console.log(response.data);
+          return response.data;
         } else {
           throw new Error(response);
         }
