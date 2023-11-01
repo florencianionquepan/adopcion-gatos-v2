fcaab884ecb157e8e3ef6053561d1056e336a839
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-31 22:52:52 -0300

    Update tabla-cuotas.component.ts

--- src/app/backoffice/components/tabla-cuotas/tabla-cuotas.component.ts
+++ src/app/backoffice/components/tabla-cuotas/tabla-cuotas.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Cuota, EstadoPago } from 'src/app/models/Cuota';
+import { GatoDetalle } from 'src/app/models/GatoDetalle';
+import { User } from 'src/app/models/user';
+import { PadrinoService } from 'src/app/services/padrino.service';
 
 @Component({
   selector: 'app-tabla-cuotas',
@@ -9,4 +12,31 @@ import { Cuota, EstadoPago } from 'src/app/models/Cuota';
 export class TablaCuotasComponent {
   @Input() cuotas:Cuota[]=[];
   EstadoPago: EstadoPago=EstadoPago.DESCONOCIDO;
+  user:User=new User();
+
+  constructor(private service:PadrinoService){}
+
+  ngOnInit(){
+    if(sessionStorage.getItem('userdetails')){
+      this.user = JSON.parse(sessionStorage.getItem('userdetails')!);
+    }
+  }
+
+  pagar(idpref:string){
+    this.service.pagarCuotaPendiente(idpref).subscribe(
+      (data)=>{
+        console.log(data);
+      }
+    )
+  }
+
+  renunciarApadrinamiento(gato:GatoDetalle){
+    //mensaje de advertenciaa!!!
+    this.service.renunciarApadrinamiento(gato, this.user.email).subscribe(
+      (data)=>{
+        console.log(data);
+        //este gatito ya no forma parte de tu listado
+      }
+    )
+  }
 }
