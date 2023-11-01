81e765b68a86555b41b16ad9ef63141a12239515
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-31 21:12:11 -0300

    Update Cuota.ts

--- src/app/models/Cuota.ts
+++ src/app/models/Cuota.ts
@@ -3,17 +3,31 @@ import { Padrino } from "./Padrino";
 
 export class Cuota{
     public id?: number;
+    public fechaCreacion?: Date;
     public fechaPago?: Date;
-    public monto?:number;
+    public montoMensual?:number;
+    public preferencia_id?:string;
+    public estadoPago?:EstadoPago;
     public padrino?:Padrino;
     public gato?:GatoDetalle;
   
-    constructor(id?: number,fechaPago?: Date,monto?: 
-        number,padrino?: Padrino,gato?:GatoDetalle) {
+    constructor(id?: number,fechaCreacion?: Date,fechaPago?: Date,
+      monto?:number,preferencia_id?:string,estadoPago?:EstadoPago,
+      padrino?: Padrino,gato?:GatoDetalle) {
       this.id = id || 0;
+      this.fechaCreacion= fechaCreacion || new Date();
       this.fechaPago = fechaPago || new Date();
-      this.monto = monto || 0;
+      this.montoMensual = monto || 0;
+      this.preferencia_id= preferencia_id || '';
+      this.estadoPago = estadoPago || EstadoPago.DESCONOCIDO;
       this.padrino = padrino || new Padrino();
       this.gato = gato || new GatoDetalle(); 
     }
+}
+
+export enum EstadoPago{
+  PENDIENTE='PENDIENTE',
+  APROBADO='APROBADO',
+  RECHAZADO='RECHAZADO',
+  DESCONOCIDO='DESCONOCIDO'
 }
