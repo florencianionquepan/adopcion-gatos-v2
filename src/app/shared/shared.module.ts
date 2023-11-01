db34069524d348e0cd3e051ab03ca52e0e0c547f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-01 19:14:40 -0300

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './components/main/main.component';
 import { CatDetailComponent } from './components/cat-detail/cat-detail.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -11,30 +10,35 @@ import { CarouselComponent } from './components/carousel/carousel.component';
 import { ErrorValidationComponent } from './components/error-validation/error-validation.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { NotificationComponent } from './components/notification/notification.component';
+import { PersonaFormComponent } from './components/persona-form/persona-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    MainComponent,
     CardsComponent,
     HomeComponent,
     CatDetailComponent,
     CarouselComponent,
     ErrorValidationComponent,
     SpinnerComponent,
-    NotificationComponent
+    NotificationComponent,
+    PersonaFormComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports:[
     HeaderComponent,
     FooterComponent,
     HomeComponent,
     ErrorValidationComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    NotificationComponent,
+    PersonaFormComponent
   ]
 })
 export class SharedModule { }
