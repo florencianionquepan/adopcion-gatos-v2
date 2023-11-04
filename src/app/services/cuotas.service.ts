dd553fa4a9b1f9b4095c6fff342baec430846547
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-04 13:06:56 -0300

    Create cuotas.service.ts

---
+++ src/app/services/cuotas.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { GatoDetalle } from '../models/GatoDetalle';
+import { Cuota } from '../models/Cuota';
+import { Padrino } from '../models/Padrino';
+import { Observable, catchError, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CuotasService {
+  public apiCuotas=`${environment.url}/cuotas`;
+
+  constructor(private http:HttpClient) { }
+
+  //la primera vez que paga una cuota
+  pagarCuota(email:string,gato:GatoDetalle):Observable<any>{
+    let cuota=new Cuota();
+    let padrino=new Padrino();
+    padrino.email=email;
+    cuota.padrino=padrino;
+    cuota.gato=gato;
+    cuota.montoMensual=gato.montoMensual;
+    return this.http.post(`${this.apiCuotas}`,cuota).pipe(
+      map((response:any) => {
+        if (response.success) {
+          //console.log(response.data);
+          window.open(response.data,'_blank');
+          return response.data;
+        } else {
+          throw new Error(response);
+        }
+      }),
+      catchError((error: any) => {
+        throw error;
+      })
+    )
+  }
+
+  pagarCuotaPendiente(preferencia_id:string):Observable<any>{
+    return this.http.get(`${this.apiCuotas}/preferencia/${preferencia_id}`).pipe(
+      map((response:any) => {
+        if (response.success) {
+          //console.log(response.data);
+          window.open(response.data,'_blank');
+        } else {
+          throw new Error(response);
+        }
+      }),
+      catchError((error: any) => {
+        throw error;
+      })
+    )
+  }
+}
