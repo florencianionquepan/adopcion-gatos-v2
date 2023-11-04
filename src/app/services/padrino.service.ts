4e8ce85dff3499d4d4ef18d03d8572f0eac9bdc8
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-04 13:07:03 -0300

    Update padrino.service.ts

--- src/app/services/padrino.service.ts
+++ src/app/services/padrino.service.ts
@@ -4,63 +4,23 @@ import { environment } from 'src/environments/environment';
 import { Cuota } from '../models/Cuota';
 import { GatoDetalle } from '../models/GatoDetalle';
 import { Padrino } from '../models/Padrino';
-import { BehaviorSubject, Observable, catchError, map } from 'rxjs';
+import { Observable, catchError, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PadrinoService {
-  public apiCuotas=`${environment.url}/cuotas`;
   public apiPadrinos=`${environment.url}/padrinos`;
 
   constructor(private http:HttpClient) {}
 
-  //la primera vez que paga una cuota
-  pagarCuota(email:string,gato:GatoDetalle):Observable<any>{
-    let cuota=new Cuota();
-    let padrino=new Padrino();
-    padrino.email=email;
-    cuota.padrino=padrino;
-    cuota.gato=gato;
-    cuota.montoMensual=gato.montoMensual;
-    return this.http.post(`${this.apiCuotas}`,cuota).pipe(
-      map((response:any) => {
-        if (response.success) {
-          //console.log(response.data);
-          window.open(response.data,'_blank');
-          return response.data;
-        } else {
-          throw new Error(response);
-        }
-      }),
-      catchError((error: any) => {
-        throw error;
-      })
-    )
-  }
 
   getCuotasByEmail(email:string):Observable<any>{
-    return this.http.get(`${this.apiCuotas}/padrino/${email}`).pipe(
+    return this.http.get(`${this.apiPadrinos}/padrino/${email}`).pipe(
       map((response:any) => {
         if (response.success) {
           //console.log(response.data);
           return response.data;
-        } else {
-          throw new Error(response);
-        }
-      }),
-      catchError((error: any) => {
-        throw error;
-      })
-    )
-  }
-
-  pagarCuotaPendiente(preferencia_id:string):Observable<any>{
-    return this.http.get(`${this.apiCuotas}/preferencia/${preferencia_id}`).pipe(
-      map((response:any) => {
-        if (response.success) {
-          //console.log(response.data);
-          window.open(response.data,'_blank');
         } else {
           throw new Error(response);
         }
