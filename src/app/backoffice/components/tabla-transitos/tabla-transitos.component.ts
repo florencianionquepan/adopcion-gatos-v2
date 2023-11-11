7e984dab14626307ec32158cbeef9e8963e14768
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-11 17:00:41 -0300

    se actualizan transitos al asignarle un nuevo gato

--- src/app/backoffice/components/tabla-transitos/tabla-transitos.component.ts
+++ src/app/backoffice/components/tabla-transitos/tabla-transitos.component.ts
@@ -1,8 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { GatoDetalle } from 'src/app/models/GatoDetalle';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { Transito } from 'src/app/models/Transito';
 import { Gato } from 'src/app/models/gato';
-import { TransitoService } from 'src/app/services/transito.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -11,24 +9,10 @@ import Swal from 'sweetalert2';
   styleUrls: ['./tabla-transitos.component.css']
 })
 export class TablaTransitosComponent {
-transitos: Transito[]=[];
+@Input() transitos: Transito[]=[];
 @Output() asignarTransitoEvent = new EventEmitter<{ id: number, nombre: string }>();
 @Input() filterTransitos='';
 
-  constructor(private service:TransitoService){
-    this.service.verTransitos()
-    .subscribe({
-      next:(response)=>{
-        //console.log(response.data);
-        this.transitos=response.data;
-      },
-      error:(e)=>{
-        //console.error("Error al enviar la ficha", e);
-        Swal.fire({title:'Error',icon:'error',text:e.mensaje})
-      }
-    }
-    )
-  }
 
   verGatos(transito:string,gatos:Gato[]){
     let html='';
--- src/app/backoffice/pages/asignacion-transito-page/asignacion-transito-page.component.ts
+++ src/app/backoffice/pages/asignacion-transito-page/asignacion-transito-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Transito } from 'src/app/models/Transito';
 import { GatosService } from 'src/app/services/gatos.service';
+import { TransitoService } from 'src/app/services/transito.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -15,9 +16,11 @@ export class AsignacionTransitoPageComponent {
   public transitoActual:Transito=new Transito();
   public icono:string='';
   filtroTransitos: string = '';
+  transitos:Transito[]=[];
 
   constructor(private actiRoute:ActivatedRoute,
-    private gatoservice:GatosService){
+    private gatoservice:GatosService,
+    private service:TransitoService){
     this.idGato=this.actiRoute.snapshot.params['id'];
     const state = history.state;
     if (state && state.nombre) {
@@ -27,6 +30,24 @@ export class AsignacionTransitoPageComponent {
     } if(state && state.icono){
       this.icono=state.icono;
     }
+  }
+
+  ngOnInit(){
+    this.getTransitos();
+  }
+
+  private getTransitos(){
+    this.service.verTransitos()
+    .subscribe({
+      next:(response)=>{
+        //console.log(response.data);
+        this.transitos=response.data;
+      },
+      error:(e)=>{
+        Swal.fire({title:'Error',icon:'error',text:e.mensaje})
+      }
+    }
+    )
   }
 
   asignarTransito(transitoData: { id: number, nombre: string }):void{
@@ -53,6 +74,7 @@ export class AsignacionTransitoPageComponent {
           next:(gatoactual)=>{
             //console.log(gatoactual);
             this.transitoActual=gatoactual.transito;
+            this.getTransitos();
             Swal.fire(
               'Genial!',
               `El gatito ${gatito} está a cargo de ${transito.nombre}`,
