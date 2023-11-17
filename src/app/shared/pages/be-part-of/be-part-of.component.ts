23f4c012143e4c45ff9608f71541b5b9107eeb9b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-16 21:50:04 -0300

    logica para el envio de solicitudes de voluntariado

--- src/app/shared/pages/be-part-of/be-part-of.component.ts
+++ src/app/shared/pages/be-part-of/be-part-of.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { User } from 'src/app/models/user';
+import { VoluntariadoService } from 'src/app/services/voluntariado.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-be-part-of',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./be-part-of.component.css']
 })
 export class BePartOfComponent {
+  user:User=new User();
+
+  constructor(private service:VoluntariadoService,
+    private authService:AuthService){
+
+  }
+
+  ngOnInit(){
+    this.user=this.authService.getUser();
+  }
+
+  solicitarVoluntariado(tipo:string):void{
+    if(!this.user.authStatus){
+      this.authService.alertLogin();
+    }
+    this.service.enviarSolicitud(this.user.email,tipo).subscribe(
+      data=>{
+        console.log(data);
+      }
+    )
+  }
 
 }
