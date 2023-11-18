c30b3bfd61bd38ee281899012084083710f44080
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-17 21:18:03 -0300

    creo componente mis-solicitudes-voluntariados

---
+++ src/app/backoffice/components/missolicitudes-voluntariados/missolicitudes-voluntariados.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { SolicitudVoluntariado } from 'src/app/models/SolicitudVoluntariado';
+import { User } from 'src/app/models/user';
+import { VoluntariadoService } from 'src/app/services/voluntariado.service';
+
+@Component({
+  selector: 'app-missolicitudes-voluntariados',
+  templateUrl: './missolicitudes-voluntariados.component.html',
+  styleUrls: ['./missolicitudes-voluntariados.component.css']
+})
+export class MissolicitudesVoluntariadosComponent {
+  solicitudes:SolicitudVoluntariado[]=[];
+  user:User=new User();
+
+  constructor(private authService:AuthService,
+              private service:VoluntariadoService){
+  this.user=this.authService.getUser();
+  }
+
+  ngOnInit(){
+    this.getSolicitudes();
+  }
+
+
+  getSolicitudes() {
+    this.service.listarByAspirante(this.user.email).subscribe(
+      (data)=>{
+        //console.log(data);
+        this.solicitudes=data;
+      }
+    )
+  }
+}
