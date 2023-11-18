692e624d234d5028fd6822385056c21709566060
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-18 12:30:31 -0300

    creación de page solicitudes voluntariados

---
+++ src/app/backoffice/pages/solicitudes-voluntariados/solicitudes-voluntariados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitudes-voluntariados',
+  templateUrl: './solicitudes-voluntariados.component.html',
+  styleUrls: ['./solicitudes-voluntariados.component.css']
+})
+export class SolicitudesVoluntariadosComponent {
+
+}
