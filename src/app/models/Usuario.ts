a20561ac935b2df5f465e9a3b8b53139246434ae
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-19 12:12:55 -0300

    Update Usuario.ts

--- src/app/models/Usuario.ts
+++ src/app/models/Usuario.ts
@@ -2,15 +2,20 @@ import { Persona } from "./Persona";
 import { Rol } from "./user";
 
 export class Usuario{
+    public id:number;
     public persona:Persona;
     public verificado:boolean;
     public bloqueado:boolean;
+    public motivo:string;
     public roles:Rol[];
 
-    constructor(persona:Persona,verificado:boolean,bloqueado:boolean,roles:Rol[]){
+    constructor(id:number,persona:Persona,verificado:boolean,
+        bloqueado:boolean,motivo:string,roles:Rol[]){
+        this.id=id,
         this.persona=persona,
         this.verificado=verificado,
         this.bloqueado=bloqueado,
+        this.motivo=motivo,
         this.roles=roles
     }
 }
